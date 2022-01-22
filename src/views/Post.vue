dd1bb402af2de6f52a6eaeda0460b584109fcee3
Atevadd <adeniyitofunmi@gmail.com>
2022-01-22 04:08:39 +0100

    chore: navbar in singlepost page

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,16 +1,48 @@
 <template>
+  <Navbar />
+  <main>
     <h2>{{ id }}</h2>
     <h2>{{ pagetitle }}</h2>
+  </main>
 </template>
 
 <script>
+import axios from "axios";
+import Navbar from "@/components/Navbar.vue";
+
 export default {
-    name: "Post",
-    props: {
-        id: Number,
-        pagetitle: String,
+  name: "Post",
+  props: ["id", "pagetitle"],
+  components: {
+    Navbar,
+  },
+  methods: {
+    getSinglePost() {
+      const userToken = localStorage.getItem("access_token");
+
+      // API config
+      const config = {
+        methods: "GET",
+        url: `https://tofumi-universal-api.herokuapp.com/api/v1/blog/${this.id}`,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userToken}`,
+        },
+      };
+
+      axios(config)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error.response);
+        });
     },
+  },
+  mounted() {
+    this.getSinglePost();
+  },
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped></style>
